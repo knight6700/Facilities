afb2e4ca3ab48abca469bd58a87af971fcd2472e
Mahmoud fares <mfnaguib@banquemisr.com>
2021-07-28 16:00:08 +0200

    Initial Commit

---
+++ Facilities/FacilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacilitiesApp.swift
+//  Facilities
+//
+//  Created by Mahmoud Fares on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FacilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
